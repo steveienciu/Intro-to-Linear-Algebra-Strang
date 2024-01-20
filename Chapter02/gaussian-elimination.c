8a53c524488c9ad7b675847a4b61c985efc272ed
steveienciu <steveienciu@yahoo.ca>
2024-01-19 20:04:33 -0500

    update

--- Chapter02/gaussian-elimination.c
+++ Chapter02/gaussian-elimination.c
@@ -3,15 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "linear_algebra.h"
 
-#define FAILURE exit(1)
-
-void mem_failure(void);
-void mem_alloc(double ***matrix, int m, int n);
-void user_input(double **matrix, int m, int n);
-void gaussian_elimination(double **matrix, int m, int n);
 void print_matrix(double **matrix, int m, int n);
-void free_matrix(double **matrix, int m);
 
 int main()
 {
@@ -31,10 +25,10 @@ int main()
 	mem_alloc(&matrix, m, m);
 
 	// accept user input 
-	user_input(matrix, m, m);
+	user_input_matrix(matrix, m, m);
 
 	// perform Gaussian elimination
-	gaussian_elimination(matrix, m, m);
+	gauss_elimination(matrix, m, m);
 
 	// output Gauss elimination matrix
 	print_matrix(matrix, m, m);
@@ -43,92 +37,6 @@ int main()
 	free_matrix(matrix, m);
 
 	return 0;
-}
-
-void mem_failure(void)
-{
-	fprintf(stderr, "\nMatrix was not properly allocated.\n");
-	FAILURE;
-}
-
-void mem_alloc(double ***matrix, int m, int n)
-{
-	int counter = 0;
-	bool isAllocated = true;
-
-	*matrix = malloc(m * sizeof(double *));
-	if (*matrix == NULL)
-	{
-		mem_failure();
-	}
-	for (int i = 0; i < n; ++i) {
-		(*matrix)[i] = malloc(n * sizeof(double));
-		// check if row can be allocated
-		if ((*matrix)[i] == NULL) {
-			isAllocated = false;
-			break;
-		}
-		++counter;
-	}
-	
-	// free memory if row not allocated 
-	if (!isAllocated) {
-		free_matrix(*matrix, counter);
-		mem_failure();
-	}
-}
-
-void user_input(double **matrix, int m, int n)
-{
-	printf("Pleae enter elements of %d x %d matrix:\n", m, m);
-	for (int i = 0; i < m; ++i) {
-		for (int j = 0; j < n; ++j) {
-			scanf("%lf", &matrix[i][j]);
-		}
-	}
-}
-
-void gaussian_elimination(double **matrix, int m, int n)
-{
-	int count = 0, i = 0, j = 0, s = 0;
-	double multiplier = 0;
-	
-	// count number of iterations for Gaussian elimination
-	for (int k = 0; k < m; ++k) {
-		count += k;
-	}
-	
-	// swap zero pivots with nonzero value below it 
-	for (int a = 0; a < m; ++a) {
-		// check if pivot is zero
-		if (matrix[a][a] == 0 && (a + 1) < m) {
-			for (int b = a + 1; b < m; ++b) {
-				if (matrix[b][a] != 0) {
-					for (int c = 0; c < n; ++c) {
-						// swap the rows
-						double temp = matrix[a][c];
-						matrix[a][c] = matrix[b][c];
-						matrix[b][c] = temp;
-					}
-					break;
-				}
-			}
-		}
-	}
-	
-	// perform Gaussian elimination
-	for (int k = 0; k < count; ++k) {
-		multiplier = -(matrix[s + 1][j] / matrix[i][i]);
-		for (int l = 0; l < n; ++l) {
-			matrix[s + 1][l] += multiplier * matrix[i][l];
-		}
-		++s;
-		if ((s + 1) == m) {
-			++j;
-			++i;
-			s = i;
-		}
-	}
 }
 
 void print_matrix(double **matrix, int m, int n)
@@ -140,12 +48,4 @@ void print_matrix(double **matrix, int m, int n)
 		}
 		printf("\n");
 	}
-}
-
-void free_matrix(double **matrix, int m)
-{
-	for (int i = 0; i < m; ++i) {
-		free(matrix[i]);
-	}
-	free(matrix);
 }
