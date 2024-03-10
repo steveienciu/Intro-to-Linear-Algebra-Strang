e042cc5730f6a414a34c3be14d4b25ea8fc8f5af
steveienciu <steveienciu@yahoo.ca>
2024-03-10 15:02:49 -0400

    new code

---
+++ Chapter05/determinants.c
@@ -0,0 +1,72 @@
+/* program that finds the determinants by multiplying the pivots of a square matrix */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "linear_algebra.h"
+
+double find_determinant(double **matrix, int m);
+
+int main()
+{
+	double **matrix, determinant;
+	int m;
+
+	// output instructions for the user to stdout 
+	printf("\nThis program will find the determinant of your square matrix.\n");
+
+	printf("Please enter the size of your square matrix: ");
+	scanf("%d", &m);
+
+	// clear input buffer
+	clear_buffer();
+
+	// ensure proper matrix size
+	if (m < 1) {
+		fprintf(stderr, "\nImproper matrix size. Terminating program.\n");
+		FAILURE;
+	}
+
+	// allocate space for the matrix
+	matrix = matrix_mem_alloc(m, m);
+	if (matrix == NULL) {
+		mem_failure();
+	}
+
+	// accept user input 
+	user_input_matrix(matrix, m, m);
+
+	// clear input buffer
+	clear_buffer();
+
+	// take the gauss elimination of the matrix
+	gauss_elimination(matrix, m, m, m, 1);
+
+	// find the determinant 
+	determinant = find_determinant(matrix, m);
+
+	// print determinant 
+	printf("The determinant of the matrix is %0.2lf.\n", determinant);
+
+	// deallocate memory
+	free_matrix(matrix, m);
+
+	return 0;
+}
+
+double find_determinant(double **matrix, int m)
+{
+	double determinant;
+	
+	// find the determinant of the matrix
+	for (int i = 0; i < m; ++i) {
+		if (i == 0) {
+			determinant = matrix[i][i];
+		}
+		else {
+			determinant *= matrix[i][i];
+		}
+	}
+
+	return determinant;
+}
