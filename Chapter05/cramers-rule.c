74b844b3a239d0205b50543a81f7cd4dbbd581f1
steveienciu <steveienciu@yahoo.ca>
2024-03-14 21:05:47 -0400

    update

--- Chapter05/cramers-rule.c
+++ Chapter05/cramers-rule.c
@@ -60,48 +60,60 @@ void find_unknowns(double **coeff_matrix, double **output_matrix, int m)
 {
 	double determinant = 0.0;
 
+	// allocate space for determinats of the unknown matrices
 	double *unknown = malloc(m * sizeof(double));
 	if (unknown == NULL) {
 		free_matrix(coeff_matrix, m); free_matrix(output_matrix, m);
-		fprintf(stderr, "\nMemory allocation failure in find_unkowns on line 73. Terminating program.\n");
+		fprintf(stderr, "\nMemory allocation failure in find_unkowns on line 67. Terminating program.\n");
 		FAILURE;
 	}
 
+	// find the determinants of the unknowns
 	for (int i = 0; i < m; ++i) {
+		// allocate space for the unknown determinant matrix
 		double **determinant_matrix = matrix_mem_alloc(m, m);
 		if (determinant_matrix == NULL) {
 			free_matrix(coeff_matrix, m); free_matrix(output_matrix, m); free(unknown);
-			fprintf(stderr, "\nMemory allocation failure in find_unknowns on line 81. Terminating program.\n");
+			fprintf(stderr, "\nMemory allocation failure in find_unknowns on line 77. Terminating program.\n");
 			FAILURE;
 		}
 
+		// set up matrix with the coefficient matrix
 		for (int j = 0; j < m; ++j) {
 			for (int k = 0; k < m; ++k) {
 				determinant_matrix[j][k] = coeff_matrix[j][k];
 			}
 		}
 
+		// swap the proper column with the output matrix, fully setting up the determinant matrix
 		for (int j = 0; j < m; ++j) {
 			determinant_matrix[j][i] = output_matrix[j][0];
 		}
 
+		// take gauss of the determinant matrix
 		gauss_elimination(determinant_matrix, m, m, m, 1);
 
 		double temp = find_determinant(determinant_matrix, m);
+		// add the determinant into the array
 		unknown[i] = temp;
 
 		free_matrix(determinant_matrix, m);
 	}
 
+	// take gauss of the coefficient matrix
+	// not sure if would be better to do this before or after finding the determinants of the unknown matrices
 	gauss_elimination(coeff_matrix, m, m, m, 1);
 
+	// find determinant of coefficient matrix
 	determinant = find_determinant(coeff_matrix, m);
+	// cannot have coefficient matrix with a zero determinant 
 	if (determinant == 0) {
 		fprintf(stderr, "\nCoefficient matrix has dependent vectors. Terminating program.\n");
 		free_matrix(coeff_matrix, m); free_matrix(output_matrix, m); free(unknown);
 		FAILURE;
 	}
 
+	// print the results of the unknowns
 	printf("The unknowns are ");
 	for (int i = 0; i < m; ++i) {
 		double temp = unknown[i] / determinant;
